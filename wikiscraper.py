52bcf12e0c31664b99d5be6dcd8a59fc236e9218
Ar <Ar>
2020-12-01 18:57:28 -0500

    added wikiscraper

---
+++ wikiscraper.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+
+
+
+url = 'https://en.wikipedia.org/wiki/Apple_Inc.'
+try:
+    response = requests.get(url)
+    print(response.status_code)
+except:
+    print('Error occured while getting the web page')
+else:
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    wiki_sidebox = soup.find('table','infobox vcard')
+
+
+website_line = wiki_sidebox.find('th',string = 'Website')
+website_block = website_line.find_parent('tr')
+company_url = website_block.find('a').get('href')
+
+stock_ticker = wiki_sidebox.find(title = 'Ticker symbol').find_parent('tr').find('li').text
+
+revenue = wiki_sidebox.find(string = 'Revenue').find_parent('tr').find('td').text
+
+employees = wiki_sidebox.find(string = 'Number of employees').find_parent('tr').find('td').text
+
